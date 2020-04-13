dc460f3eb32e53811b3c86b1a356a5df7ba375eb
Wojtek Nowak <billnewmann@gmail.com>
2020-04-13 12:04:42 +0100

    Fix column bug

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -12,7 +12,7 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.column}</h3>
+        <h3 className={styles.title}>{this.props.title}</h3>
       </section>
     )
   }
