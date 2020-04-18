26006f3bafad5ae0d924a12868726ce55e87473a
Wojtek Nowak <billnewmann@gmail.com>
2020-04-18 19:50:43 +0100

    Implemented search funtionality; modified files accordingly

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,7 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumns = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForColumns = ({cards, searchString}, columnId) => cards.filter(card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title));
 
 // action name creator
 const reducerName = 'cards';
--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,16 +1,34 @@
 // selectors
 
+// getSearchstring
+export const getSearchString = ({searchString}) => String(searchString);
+//end func
+
+// countVisibleCards
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+//end func
+
+// countAllCards
+export const countAllCards = ({cards}) => cards.length;
+//end func
+
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const SEARCH_STRING = createActionName('SEARCH_STRING');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload, type: SEARCH_STRING });
+
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case SEARCH_STRING:
+      return [action.payload];
     default:
       return statePart;
   }
 }
-
