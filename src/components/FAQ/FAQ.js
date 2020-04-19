58c41a56fff594b089acd4caa714c16e5dba0a1f
Wojtek Nowak <billnewmann@gmail.com>
2020-04-19 18:50:28 +0100

    Add FAQ component; modify app and header - husky fail

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const FAQ = () => (
+  <Container>
+    <h2>FAQ</h2>
+    <p>Lorem ipsum dolor sit amet, FAQ</p>
+  </Container>
+);
+
+export default FAQ;
