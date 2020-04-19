ea68d44f4c02f4d75d0270c96ca691a37c2d1993
Wojtek Nowak <billnewmann@gmail.com>
2020-04-19 18:05:51 +0100

    Finished Header,js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,28 @@
-/* eslint-disable no-unused-vars */
-
 import React from 'react';
 import styles from './Header.scss';
 import {NavLink, Link} from 'react-router-dom';
+import Container from '../Container/Container';
+import Icon from '../Icon/Icon';
 
 class Header extends React.Component {
 
-  //  render() { }
+  render(){
+    return (
+      <header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link to='/' className={styles.logo}>
+              <Icon name='atom' />
+            </Link>
+            <nav>
+              <NavLink exact to='/'>Home</NavLink>
+              <NavLink exact to='/info'>Info</NavLink>
+            </nav>
+          </div>
+        </Container>
+      </header>
+    );
+  }
 
 }
 
