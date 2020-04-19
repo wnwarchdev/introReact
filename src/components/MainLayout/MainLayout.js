bd24a7f218fb7bf5785e86572b4fe371652f6341
Wojtek Nowak <billnewmann@gmail.com>
2020-04-19 18:10:13 +0100

    Add Header to MainLayout

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from '../Header/Header';
 
 const MainLayout = ({children}) => (
   <div>
+    <Header/>
     {children}
-  </div>
-);
+  </div> );
 
 MainLayout.propTypes = {
   children: PropTypes.node,
