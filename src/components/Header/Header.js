f16d47605b76625a36159a5f896c1fefa425b1d5
Wojtek Nowak <billnewmann@gmail.com>
2020-04-19 16:44:44 +0100

    Start work on Header.js - husky test

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+import styles from './Header.scss';
+import {NavLink, Link} from 'react-router-dom';
+
+class Header extends React.Component {
+
+  //  render() { }
+
+}
+
+export default Header; //za Card.js
